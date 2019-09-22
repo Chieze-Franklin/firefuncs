8028ef2f24436dd3d014894922d316c44b39159c
Franklin Chieze <chieze.franklin@gmail.com>
2019-09-22 19:40:42 +0100

    support http methods for onRequest

--- options.ts
+++ options.ts
@@ -2,6 +2,12 @@ export class DatabaseOptions {
     instance?: string;
 }
 
+export class RequestOptions {
+    method?: RequestMethodTypes;
+}
+
+export type RequestMethodTypes = "connect" | "delete" | "get" | "head" | "options" | "patch" | "post" | "put" | "trace"
+
 export class ScheduleOptions {
     timeZone?: string;
 }
